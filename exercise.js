c010fd39bb356ce207ffdc8640c9e02266d2c2b6
aconkli9 <aconkli9@ford.com>
2022-03-25 15:42:46 -0400

    Make adjustments

--- exercise.js
+++ exercise.js
@@ -1,9 +1,19 @@
 function candySalesToObject(sales) {
     const [item, price, quantity] = sales
     const salesObject = { item, price, quantity }
+    salesObject.price = parseFloat(salesObject.price)
     return salesObject
 }
 
+function salesDayToObject (date, daySales) {
+    
+
+    // return {
+    // date: (dateValue)
+    sales: daySales.map(daySale => {
+        candySalesToObject(daySale)
+    })
+}
 
 module.exports = {
     candySalesToObject,
